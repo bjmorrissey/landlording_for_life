148bbe880325db0fc8eb3875e637b2fc0cd951ec
Brian <brianjmorrissey@gmail.com>
2021-07-08 12:52:07 -0500

    initial upload for template

---
+++ script.js
@@ -0,0 +1,90 @@
+// console.log("Hello")
+
+const redirect_uri = 'http://127.0.0.1:5500/index.html';
+
+const client_id = 'ef4d6cbbdec44b5585db314b4d9e059a';
+const client_secret = '475a452bc801425687de0abb5c73ac8a';
+
+const authorize = 'https://accounts.spotify.com/authorize';
+const TOKEN = 'https://accounts.spotify.com/api/token'
+
+function onPageLoad() {
+    // client_id = localStorage.getItem('client_id');
+    // client_secret = localStorage.getItem('client_secret')
+
+    if( window.location.search.length > 0 ){
+        handleRedirect();
+    }
+
+}
+
+function handleRedirect() {
+    let code = getCode();
+    fetchAccessToken(code);
+    window.history.pushState('', '', redirect_uri) 
+}
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += '&code=' + code;
+    body += '&redirect_uri=' + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += '&client_secret=' + client_secret;
+    callAuthorizationApi(body)
+}
+
+
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ':' + client_secret))
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if (data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem('access_token', access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem('refresh_token', refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText)
+        alert(this.responseText)
+    }
+} 
+
+function getCode() {
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+
+function requestAuthorization() {
+    localStorage.setItem('client_id', client_id)   
+    localStorage.setItem('client_secret', client_secret);
+
+    let url = authorize;
+    url += '?client_id=' + client_id;
+    url+= '&response_type=code';
+    url+= '&redirect_uri=' + encodeURI(redirect_uri);
+    url += '&show_dialog=true';
+    url += '&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state'
+    window.location.href = url; 
+}
+
