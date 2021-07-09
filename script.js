edf8d978bf547419df2b29ce0647b6862e47d1de
Brian <brianjmorrissey@gmail.com>
2021-07-09 13:33:55 -0500

    added API to pull from soundcloud

--- script.js
+++ script.js
@@ -1,99 +1,64 @@
-// console.log("Hello")
 const leftFooter = document.querySelector('.leftft')
-// const redirect_uri = 'http://127.0.0.1:5500/index.html';
-
-// const client_id = 'insert here';
-// const client_secret = 'insert here';
-
-// const authorize = 'https://accounts.spotify.com/authorize';
-// const TOKEN = 'https://accounts.spotify.com/api/token'
-
-// function onPageLoad() {
-//     // client_id = localStorage.getItem('client_id');
-//     // client_secret = localStorage.getItem('client_secret')
-
-//     if( window.location.search.length > 0 ){
-//         handleRedirect();
-//     }
-
-// }
-
-// function handleRedirect() {
-//     let code = getCode();
-//     fetchAccessToken(code);
-//     window.history.pushState('', '', redirect_uri) 
-// }
-
-// function fetchAccessToken(code) {
-//     let body = "grant_type=authorization_code";
-//     body += '&code=' + code;
-//     body += '&redirect_uri=' + encodeURI(redirect_uri);
-//     body += "&client_id=" + client_id;
-//     body += '&client_secret=' + client_secret;
-//     callAuthorizationApi(body)
-// }
-
-
-
-// function callAuthorizationApi(body) {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", TOKEN, true);
-//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//     xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ':' + client_secret))
-//     xhr.send(body);
-//     xhr.onload = handleAuthorizationResponse;
-// }
-
-// function handleAuthorizationResponse() {
-//     if (this.status == 200) {
-//         var data = JSON.parse(this.responseText);
-//         console.log(data);
-//         var data = JSON.parse(this.responseText);
-//         if (data.access_token != undefined) {
-//             access_token = data.access_token;
-//             localStorage.setItem('access_token', access_token);
-//         }
-//         if (data.refresh_token != undefined) {
-//             refresh_token = data.refresh_token;
-//             localStorage.setItem('refresh_token', refresh_token);
-//         }
-//         onPageLoad();
-//     }
-//     else {
-//         console.log(this.responseText)
-//         alert(this.responseText)
-//     }
-// } 
-
-// function getCode() {
-//     let code = null;
-//     const queryString = window.location.search;
-//     if (queryString.length > 0) {
-//         const urlParams = new URLSearchParams(queryString);
-//         code = urlParams.get('code')
-//     }
-//     return code;
-// }
-
-// function requestAuthorization() {
-//     localStorage.setItem('client_id', client_id)   
-//     localStorage.setItem('client_secret', client_secret);
-
-//     let url = authorize;
-//     url += '?client_id=' + client_id;
-//     url+= '&response_type=code';
-//     url+= '&redirect_uri=' + encodeURI(redirect_uri);
-//     url += '&show_dialog=true';
-//     url += '&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state'
-//     window.location.href = url; 
-// }
+const podcastArea = document.querySelector('.podcast-area')
 
 let date = new Date(); 
 
-function leftFooterUpdate() {
-    leftFooter.innerHTML = `Landlording for Life<br / >
-    © 2019-${date.getFullYear()} Landlording for Life Podcast<br>`
+leftFooter.innerHTML = `Landlording for Life<br>
+     © 2019-${date.getFullYear()} Landlording for Life Podcast<br>`
+
+let API_URL = `https://soundcloud.com/oembed?format=json&url=`
+
+
+let link = 'https://soundcloud.com/sean-morrissey-899912223/van-sturgeon-how-to-save-30-to-50-in-renovation-costs'
+
+
+async function grabShow(url) {
+     let show;
+     const API_URL = `https://soundcloud.com/oembed?format=json&url=`
+     
+     await fetch(API_URL + url)
+     .then (res => res.json()) 
+     .then (data => show = data)
+
+     const showURL = show.html
+     const visualIndex = show.html.indexOf('visual')+7
+     const srcIndex = show.html.indexOf('src')+5
+     const showLink = showURL.slice(srcIndex, (showURL.length-11)).replaceAll('true', 'false')
+     // return show;
+     console.log(show)
+     console.log(show.html)
+     console.log(showLink)
+     // show.link = showLink;
+     show.link = `<iframe width="100%" height="100" scrolling="no" frameborder="yes" src="${showLink}"></iframe>`
+     displayPodcast(show)
+     
 }
 
-leftFooterUpdate();
 
+function displayPodcast(show) {
+     let showHTML =  `
+     <div class="podcast">
+     <div class="title">${show.title}</div>
+     
+     <div class="content">
+     <img src="${show.thumbnail_url}">     
+       <p>${show.description}<br><br>
+       ${show.link}
+       
+       </p>
+
+     </div>
+     
+     
+   </div>
+     `
+
+     podcastArea.insertAdjacentHTML('afterbegin', showHTML);
+}
+
+
+grabShow('https://soundcloud.com/sean-morrissey-899912223/van-sturgeon-how-to-save-30-to-50-in-renovation-costs')
+
+grabShow('https://soundcloud.com/sean-morrissey-899912223/rod-khlief-developing-mindset-to-achieve-your-dreams')
+
+grabShow('https://soundcloud.com/sean-morrissey-899912223/david-spooner-role-of-automation-and-software-in-property-management')
